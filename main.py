cde993cea0d1516d58f74743756fd95c312b22d1
snake <admin@viasnake.com>
2021-05-26 02:30:08 +0900

    Go pyxell

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-def board_render():
-    border = []
-    i = 0
-    board_raw = open("boards/0.txt", "r")
-    lines = board_raw.readlines()
-    for line in lines:
-        print(line)
-        border.append(line)
-    board_raw.close()
-    print(border)
-    return
+import pyxel
 
-if __name__ == "__main__":
-    board_render()
+class Main:
+    def __init__(self):
+        pyxel.init(128, 128, caption="Master Robot", fps=60)
+        pyxel.load('assets/tilemap.pyxres', True, True, False, False)
+        pyxel.run(self.update, self.draw)
+
+    def update(self):
+        print("aaa")
+
+    def draw(self):
+        pyxel.cls(0)
+        pyxel.bltm(0, 0, 0, 0, 0, 16, 16)
+
+if __name__ == '__main__':
+    Main()
